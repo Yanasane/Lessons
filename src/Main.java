4a736a1d9856f3414133a90280a81943bca39ed0
Yanasane <amaravin@yandex.ru>
2019-01-14 18:34:39 +0300

    Version 0.1

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+
+        ArrayList<String> slova = new ArrayList<>();
+
+        String s = "null";
+
+        for (int x = 0; s != "Quit"; x++){
+            s = in.next();
+            slova.add(s);
+            if (slova.contains("Quit")) {
+                slova.remove("Quit");
+                break;
+            }
+        }
+        int size  = slova.size();
+        for (int x = 0; x < size; x++){
+            System.out.println(slova.get(x));
+        }
+
+
+        //          System.out.println(slova);
+
+
+
+
+
+
+
+    }
+}
